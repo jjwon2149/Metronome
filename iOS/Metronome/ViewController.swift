4c1997a7d9e847bced05eac7a638eb1ac4f4b233
jjwon2149 <jjwon2149@gmail.com>
2024-11-13 15:37:19 +0900

    Initialized Metronome Project

---
+++ iOS/Metronome/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Metronome
+//
+//  Created by 정종원 on 11/13/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        self.view.backgroundColor = .white
+    }
+
+
+}
+
